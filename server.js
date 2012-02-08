e02b6973c786cc1c3dd159bdf6672aaf5a5884e0
Nic Wolff <nic@angel.net>
2012-02-08 09:15:59 -0500

    Directory navigation and loading of "output" plists

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 var http = require('http'),
 	fs = require('fs'),
+	path = require('path'),
 	url = require('url'),
 
-	path = process.argv[2],
+	rootPath = process.argv[2],
 
 	contentTypes = {
 	    html: 'text/html',
@@ -15,22 +16,54 @@ var http = require('http'),
 
 		GET: function (resource, request, response) {
 
-			var filename = path + resource;
-			if ( resource == '/' ) {
-				resource = 'linker.html';
-				filename = resource;
-			}
-			var extension = resource.split('.').pop();
-			console.log( 'GET ' + filename );
+			var fullPath = rootPath + resource;
+			console.log( 'GET ' + resource );
 
-			fs.stat( filename, function (error, stat) {
+			if ( ! path.existsSync(fullPath) ) {
+				response.writeHead( 404, 'Not Found' );
+				response.end();
+				console.error( 'Not found: ' + resource );
+				return false;
+			}
+
+      var stats = fs.statSync(fullPath);
+
+			var masterPlist;
+
+			if ( stats.isDirectory() ) {
+				var html = '<p>Directory listing of %s</p>'.sprintf( resource );
+				fs.readdirSync( fullPath ).every( function (file) {
+					if ( file.match( /^\.DS_Store/ ) ) return true;
+					if ( masterPlist = file.match( /^(reportSession.*|Replica\.plist)$/ ) ) {
+						masterPlist = masterPlist[0];
+						resource = 'linker.html';
+						fullPath = resource;
+						return false;
+					}
+					html += '<a href="%s/">%s</a><br>'.sprintf( resource+file, file );
+					return true;
+				} );
+				if ( ! masterPlist ) {
+					response.writeHead( 200, {
+						'Content-Type': 'text/html',
+						'Content-Length': html.length
+					} );
+					response.end( html );
+					return; // from GET handler
+				}
+			}
+
+			var extension = resource.split('.').pop();
+
+			fs.stat( fullPath, function (error, stat) {
 				if (error) {
 					response.writeHead( 500, 'Error while reading %s: %s'.sprintf( resource, error ) );
 				  response.end();
 				} else {
 					var filesize = stat.size;
 					if ( resource == 'linker.html' ) {
-						var script = '<script>var fromServer = true;</script>\n\n';
+						var script = '<script>var reqURL = "%s", masterPlist = "%s";</script>\n\n'
+							.sprintf( request.url, masterPlist );
 						filesize += script.length;
 					}
 					response.writeHead( 200, {
@@ -38,7 +71,7 @@ var http = require('http'),
 						'Content-Length': filesize
 					} );
 					if ( script ) response.write( script );
-					fs.createReadStream( filename )
+					fs.createReadStream( fullPath )
 						.on( 'error', function (err) { console.error(err) } )
 						.pipe( response );
 				}
@@ -48,8 +81,8 @@ var http = require('http'),
 
 		PUT: function (resource, request, response) {
 
-			var filename = path + resource;
-			console.log( 'PUT' + filename );
+			var fullPath = rootPath + resource;
+			console.log( 'PUT ' + fullPath );
 
 			var body = '';
 			request.on('data', function (data) { body += data.toString() } );
@@ -58,9 +91,9 @@ var http = require('http'),
 
 				console.log( 'DATA: ' + body );
 
-				fs.writeFile( filename, body, 'utf8', function (err) {
+				fs.writeFile( fullPath, body, 'utf8', function (err) {
 					if (err) { console.error( 'Error during write: ' + err ) };
-					console.log( 'Saved file ' + filename );
+					console.log( 'Saved file ' + fullPath );
 					response.writeHead( 202, 'Accepted' );
 					response.end();
 				} );
@@ -70,7 +103,7 @@ var http = require('http'),
 
 	};
 
-console.log( 'Looking for files in ' + path );
+console.log( 'Looking for files in ' + rootPath );
 
 String.prototype.sprintf = function () {
 	var args = arguments, i = 0;
@@ -79,11 +112,11 @@ String.prototype.sprintf = function () {
 
 http.createServer( function (request, response) {
 	var method = request.method.toUpperCase();
-	var resource = url.parse(request.url).pathname;
+	var resource = decodeURI(url.parse(request.url).pathname);
 	if ( resource.match( /\/\.\.\//g ) ) {
 		response.writeHead( 403, 'Forbidden' );
 		response.end();
-		console.log( 'Illegal request for ' + resource );
+		console.error( 'Illegal request for ' + resource );
 		return;
 	}
 	console.log( 'Routing request for %s to %s(%s)'.sprintf( request.url, method, resource ) );
